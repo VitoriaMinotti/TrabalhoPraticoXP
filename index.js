e7b7b915b11302620485786171d918880bd491a8
VitoriaMinotti <vitorialminotti@hotmail.com>
2022-09-03 12:31:36 -0300

    completion of the project

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-let paisesPromisse = fetch("https://restcountries.com/v3.1/all");
+const fetchCountries = async () => {
+  const url = path => `https://restcountries.com/v2/${path}`
 
-paisesPromisse.then((resp) => {
-  resp.json().then((paises) => {
-    console.log(paises);
-    gerarBandeiras(paises);
-  });
-});
+  const countriesArray = []
 
-function gerarBandeiras(paises) {
-  let puxarPais = paises.map((pais) => {
-    return `<img src="${pais.flags.svg}" alt="${pais.name.common}" />`;
-  });
-  return (document.getElementById("paises").innerHTML = `${puxarPais.join("")}`);
+  countriesArray.push(await fetch(url("all")).then(res => res.json()))
+
+  let countriesLi = ''
+
+  if (countriesArray.length !== 0) {
+      countriesArray[0].forEach(country => {
+
+          const countriesEach = `
+          <li class="countryCard ${country.region}">
+              <div class="countryCard__inner">
+                  <div class="countryCard__innerFront">
+                      <img class="countryCard__flag" alt="${country.name}" src="${country.flags.png}" />
+                  </div>
+                  <div class="countryCard__innerBack">
+                      <h3>${country.name}</h3>
+                      <p>Nome nativo: ${country.nativeName}</p>
+                      <p>Capital: ${country.capital}</p>
+                      <p>Região: ${country.region}</p>
+                      <p>Sub-região: ${country.subregion}</p>
+                  </div>
+              </div>
+          </li>
+          `
+          countriesLi += countriesEach
+      })
+  }
+
+      const ul = document.getElementById('countryList__list')
+      ul.innerHTML = countriesLi
 }
+
+fetchCountries()
